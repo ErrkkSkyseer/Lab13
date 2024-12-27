8f43224442d0921d99f23a6c95dfc7d21f56b8f3
ErrkkSkyseer <lolplayer78876@gmail.com>
2024-12-27 15:52:10 +0700

    Lab 13 Completed!!!

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,13 +3,14 @@
 #include <cmath>
 using namespace std;
 
-void stat(const double[],int,double[]);
+void stat(const double[], int, double[]);
 
-int main(){
-    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
-    int N = sizeof(A)/sizeof(A[0]);
+int main()
+{
+    double A[] = {1.2, 3.5, 6.9, 7.8, 12.5, 0.5};
+    int N = sizeof(A) / sizeof(A[0]);
     double B[6];
-    stat(A,N,B);
+    stat(A, N, B);
     cout << fixed << setprecision(2);
     cout << "Arithmetic Mean = " << B[0] << endl;
     cout << "Standard Deviation = " << B[1] << endl;
@@ -18,4 +19,32 @@ int main(){
     cout << "Max = " << B[4] << endl;
     cout << "Min = " << B[5];
     return 0;
+}
+
+void stat(const double inputArray[], int size, double outputArray[])
+{
+    double sum = 0, min = inputArray[0], max = inputArray[0], geoAcc = 1, harAcc = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += inputArray[i];
+        geoAcc *= inputArray[i];
+        harAcc += 1 / inputArray[i];
+        min = inputArray[i] < min ? inputArray[i] : min;
+        max = inputArray[i] > max ? inputArray[i] : max;
+    }
+
+    outputArray[0] = sum / size;
+    double mean = outputArray[0];
+
+    double acc = 0;
+    for (int i = 0; i < size; i++)
+    {
+        acc += pow((inputArray[i] - mean), 2.0);
+    }
+
+    outputArray[1] = sqrt(acc / size);
+    outputArray[2] = pow(geoAcc, 1.0 / size);
+    outputArray[3] = size / harAcc;
+    outputArray[4] = max;
+    outputArray[5] = min;
 }
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -5,19 +5,63 @@ using namespace std;
 const int N = 30;
 const int M = 70;
 
-void updateImage(bool [][M],int,int,int);
+void updateImage(bool[][M], int, int, int);
 
-void showImage(const bool [][M]);
+void showImage(const bool[][M]);
 
 int main()
 {
     bool image[N][M] = {};
-    int s,x,y;
-    do{
+    int s, x, y;
+    do
+    {
         showImage(image);
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
-        updateImage(image,s,x,y);
-    }while(s != 0 || x != 0 || y != 0);
+        updateImage(image, s, x, y);
+    } while (s != 0 || x != 0 || y != 0);
     return 0;
+}
+
+void updateImage(bool paper[][M], int s, int x, int y)
+{
+    for (int j = 0; j < M; j++)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            if (sqrt(pow(i - x, 2.0) + pow(j - y, 2.0)) <= s - 1)
+            {
+                paper[i][j] = true;
+            }
+        }
+    }
+}
+
+void showImage(const bool paper[][M])
+{
+    for (int y = -1; y < N+1; y++)
+    {
+        for (int x = -1; x < M+1; x++)
+        {
+            if (y==-1 || y==N)
+            {
+                cout << "-";
+            }
+            else
+            {
+                if (x==-1||x==M)
+                {
+                    cout << "|";
+                }
+                else 
+                {
+                    if (paper[y][x])
+                        cout << "*";
+                    else 
+                        cout << " ";
+                }
+            }
+        }
+        cout << "\n";
+    }
 }
